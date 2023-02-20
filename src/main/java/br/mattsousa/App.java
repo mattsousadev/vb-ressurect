ee716f00ad010768d5efb4607e09eedc6b6822a2
Matheus Pinheiro de Sousa <matheus.pinheiro@itriad.org.br>
2023-02-20 18:33:17 -0400

    feat: Add I18N API

--- src/main/java/br/mattsousa/App.java
+++ src/main/java/br/mattsousa/App.java
@@ -1,28 +1,35 @@
 package br.mattsousa;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import br.mattsousa.controller.GameController;
 import br.mattsousa.model.GameCharacter;
 import br.mattsousa.util.ChanceUtil;
 
 public class App {
+
+    private static Locale locale = Locale.getDefault();
+    private static ResourceBundle messages = ResourceBundle.getBundle("message", locale);
+
     public static void main(String[] args) throws Exception {
         GameCharacter bastion = new GameCharacter(Short.valueOf("10"), Short.valueOf("10"));
         GameCharacter faulkner = new GameCharacter(Short.valueOf("10"), Short.valueOf("10"));
 
         Float hitChance = GameController.calculateHitChance(bastion, faulkner);
-        System.out.println("Hit chance: %.2f".formatted(hitChance));
+        System.out.println(messages.getString("hit.chance").formatted(hitChance));
         if (ChanceUtil.isHit(hitChance)) {
             
             GameController.executeAttack(bastion, faulkner);
             
             if (!faulkner.isAlive()) {
-                System.out.println("You win");
+                System.out.println(messages.getString("game.win"));
             } else {
-                System.out.println("You lose");
+                System.out.println(messages.getString("game.lose"));
             }
             
         } else {
-            System.out.println("Attack miss!");
+            System.out.println(messages.getString("hit.miss"));
         }
     }
 
