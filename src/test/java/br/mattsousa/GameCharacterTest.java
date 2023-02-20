6971a758db43c3caf6b6eb0006ff6bcb0de9cece
Matheus Pinheiro de Sousa <matheus.pinheiro@itriad.org.br>
2023-02-20 11:13:06 -0400

    test: ensure isAlive returns the correct values

---
+++ src/test/java/br/mattsousa/GameCharacterTest.java
@@ -0,0 +1,24 @@
+package br.mattsousa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import br.mattsousa.model.GameCharacter;
+
+public class GameCharacterTest {
+    
+    @Test
+    public void testIsAlive_shouldReturnFalse(){
+        GameCharacter bastion = new GameCharacter();
+        bastion.setLifePoints(Short.valueOf("0"));
+        Assertions.assertFalse(bastion.isAlive());
+    }
+
+    @Test
+    public void testIsAlive_shouldReturnTrue(){
+        GameCharacter bastion = new GameCharacter();
+        bastion.setLifePoints(Short.valueOf("1"));
+        Assertions.assertTrue(bastion.isAlive());
+    }
+
+}
