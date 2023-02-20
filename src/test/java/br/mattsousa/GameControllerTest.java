ac9cb0b44df3b9861770366b0400146451f0b5e3
Matheus Pinheiro de Sousa <matheus.pinheiro@itriad.org.br>
2023-02-20 11:39:21 -0400

    test: ensure executeAttack remove life points correctly

---
+++ src/test/java/br/mattsousa/GameControllerTest.java
@@ -0,0 +1,20 @@
+package br.mattsousa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import br.mattsousa.model.GameCharacter;
+
+public class GameControllerTest {
+    @Test
+    public void testExecuteAttack() {
+        GameCharacter bastion = new GameCharacter(Short.valueOf("10"), Short.valueOf("10"));
+        GameCharacter faulkner = new GameCharacter(Short.valueOf("10"), Short.valueOf("10"));
+
+        Assertions.assertEquals(Short.valueOf("10"), faulkner.getLifePoints());
+        
+        GameController.executeAttack(bastion, faulkner);
+
+        Assertions.assertEquals(Short.valueOf("0"), faulkner.getLifePoints());
+    }
+}
