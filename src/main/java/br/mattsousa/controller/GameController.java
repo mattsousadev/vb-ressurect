b5c82ce782ff21997835c76a1cc5fb9773cca4a0
Matheus Pinheiro de Sousa <matheus.pinheiro@itriad.org.br>
2023-02-20 11:57:08 -0400

    feat: check if attacker or target is alive in executeAttack

--- src/main/java/br/mattsousa/controller/GameController.java
+++ src/main/java/br/mattsousa/controller/GameController.java
@@ -4,6 +4,9 @@ import br.mattsousa.model.GameCharacter;
 
 public class GameController {
     public static void executeAttack(GameCharacter attacker, GameCharacter target) {
+        if (!attacker.isAlive() || !target.isAlive()){
+            return;
+        }
         Short damagedLifePoints = Integer.valueOf(target.getLifePoints() - attacker.getAttackPoints()).shortValue();
         if (damagedLifePoints < 0){
             damagedLifePoints = 0;
