09f15b40db83dab2316d0bacf2139cefd8c9fb88
Matheus Pinheiro de Sousa <matheus.pinheiro@itriad.org.br>
2023-02-21 01:17:20 -0400

    feat: Add a turn based combat system

--- src/main/java/br/mattsousa/App.java
+++ src/main/java/br/mattsousa/App.java
@@ -1,6 +1,9 @@
 package br.mattsousa;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Locale;
+import java.util.Queue;
 import java.util.ResourceBundle;
 
 import br.mattsousa.controller.GameController;
@@ -13,24 +16,68 @@ public class App {
     private static ResourceBundle messages = ResourceBundle.getBundle("message", locale);
 
     public static void main(String[] args) throws Exception {
-        GameCharacter bastion = new GameCharacter("Bastion", Short.valueOf("10"), Short.valueOf("10"));
-        GameCharacter faulkner = new GameCharacter("Faulkner", Short.valueOf("10"), Short.valueOf("10"));
+        Integer maxRound = 50;
 
-        Float hitChance = GameController.calculateHitChance(bastion, faulkner);
-        System.out.println(messages.getString("hit.chance").formatted(hitChance));
-        if (ChanceUtil.isHit(hitChance)) {
+        String heroName = "Bastion";
+        String villainName = "Faulkner";
+
+        GameCharacter bastion = new GameCharacter(heroName, Short.valueOf("100"), Short.valueOf("10"));
+        GameCharacter faulkner = new GameCharacter(villainName, Short.valueOf("90"), Short.valueOf("10"));
+
+        HashMap<String, GameCharacter> heroParty = new HashMap<>();
+        HashMap<String, GameCharacter> villainParty = new HashMap<>();
+
+        Queue<GameCharacter> charactersSequence = new LinkedList<>();
+
+        charactersSequence.add(bastion);
+        charactersSequence.add(faulkner);
+
+        heroParty.put(bastion.getName(), bastion);
+        villainParty.put(faulkner.getName(), faulkner);
+
+        while(maxRound > 0 && !(heroParty.isEmpty() || villainParty.isEmpty())){
+            System.out.println(messages.getString("battle.turn").formatted(maxRound));
             
-            GameController.executeAttack(bastion, faulkner);
+            GameCharacter attacker = charactersSequence.poll();
             
-            if (!faulkner.isAlive()) {
-                System.out.println(messages.getString("game.win"));
+            System.out.println(messages.getString("battle.name.and.life").formatted("Attacker", attacker.getName(), attacker.getLifePoints()));
+
+            HashMap<String, GameCharacter> targetParty = heroParty.containsKey(attacker.getName()) ? villainParty : heroParty;
+            
+            GameCharacter target = targetParty.get(heroParty.containsKey(attacker.getName()) ? villainName : heroName);
+            
+            System.out.println(messages.getString("battle.name.and.life").formatted("Target", target.getName(), target.getLifePoints()));
+
+            Float hitChance = GameController.calculateHitChance(attacker, target);
+            
+            System.out.println(messages.getString("battle.attacking"));
+            if (ChanceUtil.isHit(hitChance)) {
+                System.out.println(messages.getString("hit.hit"));
+                GameController.executeAttack(attacker, target);
+                System.out.println(messages.getString("hit.damage").formatted(attacker.getAttackPoints()));
+            }else{
+                System.out.println(messages.getString("hit.miss").formatted(attacker.getName(), target.getName()));
+            }
+
+            if(!target.isAlive()){
+                System.out.println(messages.getString("battle.defeated").formatted(target.getName()));
+                targetParty.remove(target.getName());
             } else {
-                System.out.println(messages.getString("game.lose"));
+                charactersSequence.add(target);
             }
             
-        } else {
-            System.out.println(messages.getString("hit.miss"));
+            System.out.println();
+            maxRound--;
         }
+
+        if(heroParty.isEmpty()){
+            System.out.println(messages.getString("game.lose"));
+        }else if(villainParty.isEmpty()){
+            System.out.println(messages.getString("game.win"));
+        }else{
+            System.out.println(messages.getString("game.draw"));
+        }
+
     }
 
 }
